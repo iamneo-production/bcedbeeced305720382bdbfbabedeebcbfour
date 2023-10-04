d0db7a6f6e3f3a7fe240e168dc7dcf8e1b2657a8
Iamneo <techadmin@iamneo.ai>
2023-10-04 10:28:58 +0000

    Backup Commit

--- Project/src/test/java/Runner/RunTest.java
+++ Project/src/test/java/Runner/RunTest.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/Features",
+@CucumberOptions(features="Features",
 				glue = "StepDefinition")
 public class RunTest {
 }
